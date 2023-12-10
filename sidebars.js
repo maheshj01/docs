8edc3ae82a6006a216cea8c1e5b8dacfcfc8665c
mahesh jamdade <maheshmn121@gmail.com>
2023-12-10 00:24:47 -0500

    structure the docs

--- sidebars.js
+++ sidebars.js
@@ -13,17 +13,18 @@
 
 module.exports = {
   "docs": [
-    "introduction",
-    "installation",
-    "features",
-    "types",
-    "navbarnotifier",
-    "routes-and-navigation",
-    "snackbars",
-    "showcase",
-    "background",
-    "properties",
-    "contributing",
-    "release-notes",
+    "navbar_router/introduction",
+    "navbar_router/installation",
+    "navbar_router/features",
+    "navbar_router/types",
+    "navbar_router/navbarnotifier",
+    "navbar_router/routes-and-navigation",
+    "navbar_router/snackbars",
+    "navbar_router/showcase",
+    "navbar_router/background",
+    "navbar_router/properties",
+    "navbar_router/contributing",
+    "navbar_router/release-notes",
+    "git-tutorial/introduction",
   ]
 }
