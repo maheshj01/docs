9511f40c5c7dcfdf01bfe38e87e9bf7776f6c141
mahesh jamdade <maheshmn121@gmail.com>
2024-04-21 22:52:10 -0400

    minor updates

--- src/components/common/NavLink.js
+++ src/components/common/NavLink.js
@@ -1,11 +1,14 @@
 // create a custom link componnet
 import React from 'react';
-
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 export default function NavLink(props) {
+    const { siteConfig } = useDocusaurusContext();
+    const isDarkTheme = siteConfig.isDarkTheme;
     return (
-        <a href={props.to} className={props.className}
+        <a href={props.to}
+            className={`${props.className} ${isDarkTheme ? 'text-primary' : 'text-blue-600'}`}
             target={props.isNewTab ? '_blank' : '_self'}
         // rel={props.isNewTab ? 'noopener noreferrer' : ''}
-        >{props.text}</a>
+        > {props.text}</a >
     );
 }
